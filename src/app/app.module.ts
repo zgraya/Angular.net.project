387efe2f7668d6dd113736836b3b7a71786138ea
zgraya <raven.soshir@gmail.com>
2021-02-16 14:00:02 +0100

    Create server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
 import { LineChartComponent } from './charts/line-chart/line-chart.component';
 import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 import {ChartsModule} from 'ng2-charts';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import {ChartsModule} from 'ng2-charts';
     SectionHealthComponent,
     BarChartComponent,
     LineChartComponent,
-    PieChartComponent
+    PieChartComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
