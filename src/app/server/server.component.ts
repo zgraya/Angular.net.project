1f8f3ca2767239ddb9d1c9311501290ea34e377e
zgraya <raven.soshir@gmail.com>
2021-02-18 10:18:59 +0100

    server component updated

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,9 +10,32 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText: string;
+
   @Input() serverInput: Server;
 
   ngOnInit(): void {
+    this.getServerAction(this.serverInput.isOnline);
   }
 
+  // tslint:disable-next-line:typedef
+  getServerAction(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66bb6a';
+      this.buttonText = 'Shut Down';
+    }else{
+      this.serverInput.isOnline = false;
+      this.color = '#ff6b6b';
+      this.buttonText = 'Start';
+
+    }
+  }
+
+  // tslint:disable-next-line:typedef
+  toggleStatus(onlineStatus: boolean) {
+    console.log(this.serverInput.name, ': ', onlineStatus);
+    this.serverInput.isOnline = !this.serverInput.isOnline;
+  }
 }
